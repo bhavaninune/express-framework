8a8a414bb500cad1071f6a529b57634279c11b38
bhavaninune <142501709+bhavaninune@users.noreply.github.com>
2023-12-03 05:16:10 +0530

    Add files via upload
    handling different routes

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const http = require('http');
+//const http = require('http');
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('in the middleware!');
+app.use('/',(req, res, next) => {
+  console.log('This always runs!');
   next(); // This is missing in your original code
 });
-
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
+  console.log('in another middleware!');
+  res.send('<h1>The "Add product" page<h1>');
+});
+app.use('/',(req, res, next) => {
   console.log('in another middleware!');
   res.send('<h1>hello from express!<h1>');
 });
 
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
 /*server.listen(3001, () => {
   console.log('Server is listening on port 3001');
 });*/
-server.listen(3001);
+app.listen(3001);
 
