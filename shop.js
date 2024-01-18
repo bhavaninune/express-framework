78f6b2886556e3cb5c7bc9042903aa3643c3812e
bhavaninune <142501709+bhavaninune@users.noreply.github.com>
2024-01-18 17:11:59 +0530

    Add files via upload

--- shop.js
+++ shop.js
@@ -1,48 +1,17 @@
-const Product = require('../models/product');
+const path = require('path');
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
-};
+const express = require('express');
 
-exports.getProducts=(req,res,next)=>{
-  const prodId=req.params.productId;
-  console.log(prodId);
-  res.redirect('/');
+const productcontroller= require('../controlers/shop')
 
-};
-exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
-};
+const router = express.Router();
 
-exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
-};
+router.get('/',ShopController.getIndex);
+router.get('/products',ShopController.getProduct);
+router.get('/products/:productId',ShopController.getProduct);
+router.get('/cart',ShopController.getCart);
+router.get('/orders',ShopController.getOrders);
+router.get('/checkout',ShopController.getCheckout);
 
-exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
-};
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
-};
+module.exports = router;
